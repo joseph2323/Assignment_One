e4d47f6e7820133902b955c97c112254c265f00a
joseph2323 <josephfootball@hotmail.com>
2015-11-02 13:48:08 +0000

    Update a1-server.js

--- a1-server.js
+++ a1-server.js
@@ -30,6 +30,7 @@ function handleRequest(request, response){
     // Send user the server response 
     response.end('Assignment One. Expected Sum of 6 + 3 is 9, Actual Sum returned by program is : ' + sum);
     console.re.log("The answer is " + sum);
+    console.re.log("The augend value is " + augend);
     // ASSIGNMENT
     // 1. Add a debug message which ouputs the sum of the simple equation above. Values are available in the augend, addend 
     // and sum (global) variables
@@ -42,7 +43,8 @@ var server = http.createServer(handleRequest);
 // Lets start our server
 server.listen(server_port, server_ip_address, function(){
     // This is the callback function which triggered when server is successfully listening (active).
-    
+    console.re.log("Your server has started .... fairplay to ya");
+    console.re.log("My server ip address is: " + server_ip_address + ":" + server_port);
     // ASSIGNMENT
     // 3. Add a debug message which ouputs a message indicating the server is started (listening for user requests).
     // 4. Add a debug message which ouputs your server ip address and your server port number. Your server ip address 
